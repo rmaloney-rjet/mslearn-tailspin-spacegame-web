42877803db97354c562a518606390149f6a4a7bf
rmaloney-rjet <rsmaloney@gmail.com>
2019-08-27 21:00:27 -0400

    Bump Models package to 2.0.0 p3

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -9,7 +9,7 @@ namespace TailSpin.SpaceGame.Web
         {
             CreateWebHostBuilder(args).Build().Run();
         }
-
+        //test
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
